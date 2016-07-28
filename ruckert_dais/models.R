e48c0aec79bd6c3d78fe1dc772dec7c53779485c
Robert William Fuller <hydrologiccycle@gmail.com>
2016-07-28 14:45:24 -0400

    updates for Fortran MCMC plots

--- ruckert_dais/models.R
+++ ruckert_dais/models.R
@@ -20,47 +20,11 @@ if (exists("daisassimctx") && exists("fortran", env=daisassimctx)) {
 source("roblib.R")
 
 
-if (useFmodel) {
-    dynReload("../fortran/dais", makevars='PKG_FCFLAGS="-I../fortran -J../fortran"',
-        srcname=paste("../fortran/src/", c("dais.f90", "run_dais.f90", "global.f90"), sep=""))
-    source("daisF.R")
-
-    iceflux <- function(iceflux, forcings, standards)
-    {
-        Volume_F <- daisF(
-          tstep = 1,
-          b0    = iceflux[10],
-          slope = iceflux[11],
-          mu    = iceflux[3],
-          h0    = iceflux[8],
-          c     = iceflux[9],
-          P0    = iceflux[5],
-          kappa = iceflux[6],
-          nu    = iceflux[4],
-          f0    = iceflux[7],
-          gamma = iceflux[1],
-          alpha = iceflux[2],
-          Tf    = -1.8,             #Freezing temperature of sea water
-          rho_w = 1030,             #Density of sea water [g/cm^3]
-          rho_i = 917,              #Density of ice water [g/cm^3]
-          rho_m = 4000,             #Density of rock [g/cm^3]
-          Toc_0 = 0.72,             #Present day high latitude ocean subsurface temperature [K]
-          Rad0  = 1.8636e6,         #Steady state AIS radius for present day Ta and SL [m]
-          Ta     = forcings[, 1], 
-          SL     = forcings[, 4],
-          Toc    = forcings[, 2],
-          dSL    = forcings[, 3])
-
-        return (Volume_F)
-    }
-}
-
-
-if (useCmodel) {
+if (useCmodel || useFmodel) {
     dynReload("dais_alex",   srcname=c("dais_alex.c",   "r.c"), extrasrc="r.h")
     dynReload("dais_kelsey", srcname=c("dais_kelsey.c", "r.c"), extrasrc="r.h")
 
-    if (alex) {
+    if (alex || useFmodel) {
         daisCmodel <- "daisAlexOdeC"
     } else {
         daisCmodel <- "daisKelseyOdeC"
@@ -108,5 +72,41 @@ if (useCmodel) {
     {
         out <- iceflux_RHF(iceflux, forcings, standards)
         return (out$SLE)
+    }
+}
+
+
+if (useFmodel) {
+    dynReload("../fortran/dais", makevars='PKG_FCFLAGS="-I../fortran -J../fortran"',
+        srcname=paste("../fortran/src/", c("dais.f90", "run_dais.f90", "global.f90"), sep=""))
+    source("daisF.R")
+
+    iceflux <- function(iceflux, forcings, standards)
+    {
+        Volume_F <- daisF(
+          tstep = 1,
+          b0    = iceflux[10],
+          slope = iceflux[11],
+          mu    = iceflux[3],
+          h0    = iceflux[8],
+          c     = iceflux[9],
+          P0    = iceflux[5],
+          kappa = iceflux[6],
+          nu    = iceflux[4],
+          f0    = iceflux[7],
+          gamma = iceflux[1],
+          alpha = iceflux[2],
+          Tf    = -1.8,             #Freezing temperature of sea water
+          rho_w = 1030,             #Density of sea water [g/cm^3]
+          rho_i = 917,              #Density of ice water [g/cm^3]
+          rho_m = 4000,             #Density of rock [g/cm^3]
+          Toc_0 = 0.72,             #Present day high latitude ocean subsurface temperature [K]
+          Rad0  = 1.8636e6,         #Steady state AIS radius for present day Ta and SL [m]
+          Ta     = forcings[, 1],
+          SL     = forcings[, 4],
+          Toc    = forcings[, 2],
+          dSL    = forcings[, 3])
+
+        return (Volume_F)
     }
 }
