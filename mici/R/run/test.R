43cde987eba0867ddd1220ae6592d5c7a5f95380
Robert William Fuller <hydrologiccycle@gmail.com>
2016-08-16 21:08:07 -0400

    clean up test code

--- mici/R/run/test.R
+++ mici/R/run/test.R
@@ -1,6 +1,7 @@
-set.seed(9);
+set.seed(9)
 source("assim_dais.R")
 daisConfigAssim()
 daisRunFit()
 daisRunAssim(nbatch=1e4)
-#save.image("DAIS_MCMC_Rversioncalibration.RData")
+daisRunPredict(nbatch=1e3)
+save.image("DAIS_MCMC_Rversioncalibration_test.RData")
