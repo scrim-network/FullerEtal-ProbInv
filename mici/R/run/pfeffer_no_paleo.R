b6a70d997b35fd3a6a94d6af6461d2ca57ad86d8
Robert William Fuller <hydrologiccycle@gmail.com>
2016-09-15 13:49:22 -0400

    more runs

---
+++ mici/R/run/pfeffer_no_paleo.R
@@ -0,0 +1,6 @@
+source("calib.R")
+daisConfigAssim(fast_dyn=T, pfeffer=T, paleo=F)
+daisRunFit()
+daisRunAssim(nbatch=5e5)
+daisRunPredict()
+save.image("DAIS_MCMC_Rversioncalibration_pfeffer_no_paleo.RData")
