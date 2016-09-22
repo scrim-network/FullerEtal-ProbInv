c1c1f5785c25493bf9c0c9a8bdaf775088fa680b
Robert William Fuller <hydrologiccycle@gmail.com>
2016-09-22 10:57:52 -0400

    it figures

--- mici/R/makefig.R
+++ mici/R/makefig.R
@@ -15,13 +15,34 @@
 #
 # makefig.R
 #
-outfiles <- F
+
+outfiles <- T
+year <- 2100
+
 
 source('pdfplot.R')
 source('plotutils.R')
 source('roblib.R')  # burnedInd()
 
 bchain <- daisctx$chain[ burnedInd(daisctx$chain), ]
+
+
+load("uniform")
+pr1 <- prdaisctx
+load("normal")
+pr2 <- prdaisctx
+load("beta")
+pr3 <- prdaisctx
+
+
+if (0) {
+
+prchain <- cbind( prdaisctx$prchain[, as.character(year) ] )
+colnames(prchain) <- paste("Projected AIS Volume Loss in", year, "[SLE m]")
+pdfplot(mcmcChain=prchain, units="m", chartRow=1, chartCol=1,
+    outfiles=outfiles, burnin=F, width=8, height=8,
+    caption=paste("Probability density function of AIS volume loss in year",
+        year))
 
 pdfplot(assimctx=daisctx, outfiles=outfiles)
     
@@ -33,5 +54,8 @@ plot.pairs(bchain)
 
 #newDev("margin2", outfile=outfiles)
 #plot.marginals(bchain[ , 10:ncol(bchain)])
+
+}
+
 
 finDev()
