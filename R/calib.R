1ea54e18d2adf3164599ba1fb79583b7f68ad764
Robert William Fuller <hydrologiccycle@gmail.com>
2017-02-18 15:03:49 -0500

    switch back to narrow priors

--- R/calib.R
+++ R/calib.R
@@ -262,7 +262,7 @@ source("../fortran/R/dais_fastdynF.R")
 
 # cModel can be either rob, kelsey, or NULL right now.  NULL selects the Fortran model.
 daisConfigAssim <- function(
-    cModel="rob", fast_dyn=T, rob_dyn=F, fast_only=F, wide_prior=T,
+    cModel="rob", fast_dyn=T, rob_dyn=F, fast_only=F, wide_prior=F,
     instrumental=F, paleo=F, expert="pfeffer", prior="uniform",
     gamma_pri=T, variance=T, all_predict=F, heaviside=F, assimctx=daisctx)
 {
